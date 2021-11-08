b7524e402e573408b1f230837cc2a3118815faa7
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-08 12:02:09 -0600

    Use real Families from SQL

--- DemoMvc/Services/IFamiliesRepository.cs
+++ DemoMvc/Services/IFamiliesRepository.cs
@@ -1,4 +1,6 @@
+using DemoMvc.Data;
 using DemoMvc.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +15,20 @@ namespace DemoMvc.Services
 
     public class DatabaseFamilyRepository : IFamilyRepository
     {
+        private readonly DemoDbContext _context;
+
+        public DatabaseFamilyRepository(DemoDbContext context)
+        {
+            _context = context;
+        }
+
         public async Task<List<Family>> GetAll()
         {
-            return new List<Family>
-            {
-                new Family { Id = 45, Name = "Jetsons" },
-            };
+            //return new List<Family>
+            //{
+            //    new Family { Id = 45, Name = "Jetsons" },
+            //};
+            return await _context.Families.ToListAsync();
         }
     }
 }
