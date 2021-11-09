55e870e6e8919b6b7ed1cb666e3fef0b3ac66f64
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-09 11:52:00 -0600

    Add [Display(Name)]

--- DemoMvc/Models/Person.cs
+++ DemoMvc/Models/Person.cs
@@ -10,15 +10,18 @@ namespace DemoMvc.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         [Required]
         [StringLength(50)]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [Required]
         [StringLength(50)]
         public string LastName { get; set; }
 
         // If you leave this out, it will probably figure out that you need one
+        [Display(Name = "Family")]
         public int FamilyId { get; set; }
 
         // Navigation Property that makes FamilyId a Foreign Key
