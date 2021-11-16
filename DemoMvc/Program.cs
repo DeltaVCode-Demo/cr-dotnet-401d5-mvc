61ac100df2d7ed22d13dc49edb79dd042a491398
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-16 10:29:00 -0600

    Migrate Database on Startup

--- DemoMvc/Program.cs
+++ DemoMvc/Program.cs
@@ -2,18 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DemoMvc.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace DemoMvc
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +25,16 @@ namespace DemoMvc
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static IHost MigrateDatabase(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+
+            // Not using because the scope will automatically dispose
+            var db = scope.ServiceProvider.GetRequiredService<DemoDbContext>();
+            db.Database.Migrate();
+
+            return host;
+        }
     }
 }
