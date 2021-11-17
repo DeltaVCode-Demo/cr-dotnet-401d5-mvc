e257df847ed41c4fa2293878b46c5fa24e988be9
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-17 12:13:16 -0600

    Scaffold a single Razor Page from EF

---
+++ DemoMvc/Pages/Testing.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DemoMvc.Data;
+using DemoMvc.Models;
+
+namespace DemoMvc.Pages
+{
+    public class TestingModel : PageModel
+    {
+        private readonly DemoMvc.Data.DemoDbContext _context;
+
+        public TestingModel(DemoMvc.Data.DemoDbContext context)
+        {
+            _context = context;
+        }
+
+        public Person Person { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Person = await _context.Persons
+                .Include(p => p.Family).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Person == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
