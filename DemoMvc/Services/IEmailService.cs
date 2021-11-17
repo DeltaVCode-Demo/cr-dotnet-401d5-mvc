7b3aa56604049b72411eea8cd4039e5f2e1f53c8
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-17 10:42:35 -0600

    Initial SendGridEmailService

---
+++ DemoMvc/Services/IEmailService.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMvc.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmail(string to, string subject, string bodyPlain, string bodyHtml);
+    }
+
+    public class SendGridEmailService : IEmailService
+    {
+        private IConfiguration Configuration { get; }
+
+        public SendGridEmailService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public async Task SendEmail(string toEmail, string subject, string plainTextContent, string htmlContent)
+        {
+            var apiKey = Configuration["SendGrid:ApiKey"]
+                ?? throw new InvalidOperationException("SendGrid:ApiKey not found!");
+
+            var client = new SendGridClient(apiKey);
+            //var from = new EmailAddress("test@example.com", "Example User");
+            var from = new EmailAddress("keith+401d5@deltavcodeschool.com");
+            //var subject = "Sending with SendGrid is Fun";
+            // var to = new EmailAddress("test@example.com", "Example User");
+            var to = new EmailAddress(toEmail);
+            //var plainTextContent = "and easy to do anywhere, even with C#";
+            //var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
